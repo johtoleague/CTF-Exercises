81771e7ea4bd768f2f89569c2cf82ee58b53046a
johtoleague <jon.doanne@gmail.com>
2022-11-30 18:11:11 +0000

    fix

--- cryptopals/chal2/chal2.py
+++ cryptopals/chal2/chal2.py
@@ -7,7 +7,7 @@ def _bytes_xor(a: bytes, b: bytes, quiet=True, check_lens=False) -> bytes:
 
 def bytes_xor(*args: bytes, quiet=True, check_lens=False):
     assert len(args) > 0 #looks for errors will kill this application 
-    resutls = args[0]
+    result = args[0]
     for arg in args[1:]:
         result = _bytes_xor(result, arg, quiet=quiet, check_lens=check_lens)
     return result
@@ -17,4 +17,7 @@ if __name__ == '__main__':
     a = bytes.fromhex("1c0111001f010100061a024b53535009181c")
     b = bytes.fromhex("686974207468652062756c6c277320657965")
     result = bytes_xor(a, b, quiet=False)
+    
+    print(f"{result=}")
+    print(f"{result.hex()}")
     
